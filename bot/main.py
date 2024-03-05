1698963952ab2ec48ceea10d5dbf45b93926b4cd
k1cker666 <pomoymu@gmail.com>
2024-03-05 21:40:16 +0300

    feat tgbot: echo-bot

---
+++ bot/main.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from src.handlers import start, help, echo
+    
+def main():
+    application = Application.builder().token("7148494691:AAFgTWiU8919YwGHw6l8LTAAa3nxuhGUmO4").build()
+
+    application.add_handler(CommandHandler("start", start.start))
+    application.add_handler(CommandHandler("help", help.help_command))
+
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo.echo))
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
---
+++ bot/src/handlers/help.py
@@ -0,0 +1,5 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Help!")
