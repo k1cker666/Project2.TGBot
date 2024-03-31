9669f3f6ba0b5ad0186f78af05dd55f6034d2760
k1cker666 <pomoymu@gmail.com>
2024-03-31 13:35:16 +0300

    feat tgbot: unwork buttons

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -1,18 +1,39 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from src.handlers import start, help, echo
+from telegram import (
+    Update,
+    ReplyKeyboardMarkup,
+    KeyboardButton
+    )
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes
+    )
+from src.handlers import help, echo
+from functools import partial
+# from dependencies import Dependencies
 
 def start_bot(dependencies):
     application = Application.builder().token("7148494691:AAFgTWiU8919YwGHw6l8LTAAa3nxuhGUmO4").build()
-
-    application.add_handler(CommandHandler("start", start))
+    
+    start_mes = partial(start, dependencies = dependencies)
+    
+    application.add_handler(CommandHandler("start", start_mes))
     application.add_handler(CommandHandler("help", help.help_command))
     
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo.echo))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
     
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE, dependencies):
     user = update.effective_user
     await update.message.reply_html(
-        rf"Привет {user.mention_html()}, я бот повторяющий сообщения!")
+        rf"Привет {user.mention_html()}, я бот, которы поможет тебе выучить иностранные слова!")
+    buttons = KeyboardButton('Авторизация')
+    but_reply = ReplyKeyboardMarkup(buttons)
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Выбери действие",
+        reply_markup=but_reply
+    )
--- bot/src/dependencies.py
+++ bot/src/dependencies.py
@@ -1,14 +1,26 @@
 from src.db import psql, redis
+from src.components.start_handler import StartHedler
 
 class Dependencies:
 
-    def __init__(self, psql_connect, redis_connect):
+    def __init__(
+        self,
+        psql_connect,
+        redis_connect,
+        start_hendler
+    ):
         self.psql_connect = psql_connect
         self.redis_connect = redis_connect
+        self.start_hendler = start_hendler
     
 class DependenciesBuilder:
     
     def build():
         psql_connect = psql.create_connection()
         redis_connect = redis.create_connectrion()
-        return Dependencies(psql_connect=psql_connect, redis_connect=redis_connect)
+        start_hendler = StartHedler
+        return Dependencies(
+            psql_connect=psql_connect,
+            redis_connect=redis_connect,
+            start_hendler=start_hendler
+        )
