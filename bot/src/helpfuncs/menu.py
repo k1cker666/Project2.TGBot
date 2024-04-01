257ca6fdffcd8d154a34b26c53c09f8a46303ea3
k1cker666 <pomoymu@gmail.com>
2024-04-02 01:03:58 +0300

    test tgbot: new test for build_menu

--- bot/src/helpfuncs/menu.py
+++ bot/src/helpfuncs/menu.py
@@ -3,9 +3,7 @@ def build_menu(buttons, n_cols,
                footer_buttons=None):
     menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
     if header_buttons:
-        menu.insert(0, [header_buttons])
+        menu.insert(0, header_buttons)
     if footer_buttons:
-        menu.append([footer_buttons])
+        menu.append(footer_buttons)
     return menu
-
-print(build_menu(['a','v'], -1))
--- bot/tests/test_menu.py
+++ bot/tests/test_menu.py
@@ -2,16 +2,21 @@ import pytest
 from src.helpfuncs.menu import build_menu
 
 @pytest.mark.parametrize(
-    "array, cols, res",
+    "array, cols, head, foot, res",
     [
-        (['a'], 1, [['a']]),
-        (['a'], 2, [['a']]),
-        (['a', 'b'], 1, [['a'], ['b']]),
-        (['a', 'b'], 2, [['a', 'b']]),
-        (['a', 'b', 'c'], 1, [['a'], ['b'], ['c']]),
-        (['a', 'b', 'c'], 2, [['a', 'b'], ['c']]),
-        (['a', 'b', 'c'], 3, [['a', 'b', 'c']])
+        (['a'], 1, None, None, [['a']]),
+        (['a'], 1, ['b'], None, [['b'], ['a']]),
+        (['a'], 1, ['b', 'c'], None, [['b', 'c'], ['a']]),
+        (['a'], 1, None, ['b'], [['a'], ['b']]),
+        (['a'], 1, None, ['b', 'c'], [['a'], ['b', 'c']]),
+        (['a'], 2, None, None, [['a']]),
+        (['a'], 2, ['b'], None, [['b'], ['a']]),
+        (['a'], 2, None, ['b'], [['a'], ['b']]),
+        (['a', 'b'], 1, None, None, [['a'], ['b']]),
+        (['a', 'b'], 2, None, None, [['a', 'b']]),
+        (['a', 'b', 'c'], 2, None, None, [['a', 'b'], ['c']]),
+        (['a', 'b', 'c'], 3, None, None, [['a', 'b', 'c']])
     ]
 )
-def test_menu(array, cols, res):
-    assert build_menu(array, cols) == res
+def test_menu(array, cols, head, foot, res):
+    assert build_menu(array, cols, head, foot) == res
