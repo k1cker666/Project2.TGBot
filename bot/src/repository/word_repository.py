104a2c9c24c83d3fc492d6d99006f3bc5e7f0275
k1cker666 <pomoymu@gmail.com>
2024-04-06 21:48:42 +0300

    refactor tgbot: row_factory for fetch

--- bot/src/repository/word_repository.py
+++ bot/src/repository/word_repository.py
@@ -1,6 +1,6 @@
 import psycopg
+from psycopg.rows import class_row
 from src.models.word import Word
-
 class WordRepository:
     
     connection: psycopg.Connection
@@ -9,18 +9,10 @@ class WordRepository:
         self.connection = connection
         
     def fetch(self, id):
-        with self.connection.cursor() as cur:
+        with self.connection.cursor(row_factory=class_row(Word)) as cur:
             cur.execute(
-                "select * from tgbot.words where word_id = %s",
+                "select * from words where word_id = %s",
                 (id,)
             )
             result = cur.fetchone()
-            if result != None:
-                return Word(
-                    word_id = result[0],
-                    language = result[1],
-                    level = result[2],
-                    word = result[3]
-                )
-            else:
-                return result
+            return result
