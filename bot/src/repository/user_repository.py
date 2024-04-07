bc79e36f736a960eae3a7ada8d1b88cd76ba073e
k1cker666 <pomoymu@gmail.com>
2024-04-07 13:02:45 +0300

    feat tgbot: add fetch() in userrepo

--- bot/src/repository/user_repository.py
+++ bot/src/repository/user_repository.py
@@ -1,4 +1,6 @@
 import psycopg
+from src.models.user import User
+from psycopg.rows import class_row
 
 class UserRepository:
     
@@ -6,3 +8,12 @@ class UserRepository:
     
     def __init__(self, connection):
         self.connection = connection
+        
+    def fetch(self, id: int) -> User:
+        with self.connection.cursor(row_factory=class_row(User)) as cur:
+            cur.execute(
+                "select * from users where user_id = %s",
+                (id,)
+            )
+            result = cur.fetchone()
+            return result
