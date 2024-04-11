fd020728892f22f63b14869e98a9b54657a72f6f
k1cker666 <pomoymu@gmail.com>
2024-04-11 13:05:40 +0300

    feat tgbot: add config in deps

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -12,10 +12,13 @@ from telegram.ext import (
 from src.handlers import help, echo
 from functools import partial
 import json
+from loguru import logger
 
 
 def start_bot(deps):
     application = Application.builder().token("7148494691:AAFgTWiU8919YwGHw6l8LTAAa3nxuhGUmO4").build()
+    
+    logger.info('Application was started')
     
     start_with_deps = partial(start, deps = deps)
     callback_handler_with_deps = partial(callback_handler, deps = deps)
--- bot/src/dependencies.py
+++ bot/src/dependencies.py
@@ -1,5 +1,6 @@
 from src.db import psql, redis
 from src.components.start_handler import StartHandler
+from src.components.config import load_config, Config
 from redis import Redis
 from src.repository.word_repository import WordRepository
 from src.repository.user_repository import UserRepository
@@ -10,18 +11,21 @@ class Dependencies:
     start_handler: StartHandler
     word_repository: WordRepository
     user_repository: UserRepository
+    config: Config
     
     def __init__(
         self,
         redis_connect,
         start_handler,
         word_repository,
-        user_repository
+        user_repository,
+        config
     ):
         self.redis_connect = redis_connect
         self.start_handler = start_handler
         self.word_repository = word_repository
         self.user_repository = user_repository
+        self.config = config
     
     def close(self):
         self.redis_connect.close()
@@ -36,9 +40,11 @@ class DependenciesBuilder:
         start_handler = StartHandler()
         word_repository = WordRepository(connection=psql_connect)
         user_repository = UserRepository(connection=psql_connect)
+        config = load_config()
         return Dependencies(
             redis_connect=redis_connect,
             start_handler=start_handler,
             word_repository=word_repository,
-            user_repository=user_repository
+            user_repository=user_repository,
+            config = config
         )
