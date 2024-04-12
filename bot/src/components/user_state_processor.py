9e2e389c013478ee28d96d5b8616913f2bad6abb
k1cker666 <pomoymu@gmail.com>
2024-04-12 11:09:28 +0300

    feat tgbot: add func for userstate

--- bot/src/components/user_state_processor.py
+++ bot/src/components/user_state_processor.py
@@ -1,19 +1,30 @@
+from datetime import timedelta
 from enum import Enum, auto
+import redis
+
+class State(Enum):
+    lesson_active = auto()
+    lesson_inactive = auto()
 
 class UserStateProcessor:
     
-    class State(Enum):
-        lesson_active = auto()
-        lesson_inactive = auto()
+    conn: redis.Redis
     
-    def set_state(self, user_id, state):
-        pass
+    def __init__(self, connecton: redis.Redis):
+        self.conn = connecton
     
-    def get_state(self, user_id) -> State:
-        pass
+    def set_state(self, user_id: str, state: State):
+        self.conn.hset(f'{user_id}', 'state', state.name)
+        ttl = timedelta(minutes=10)
+        self.conn.expire(name=f'{user_id}', time=ttl)
     
-    def set_data(self, user_id, data):
-        pass
+    def get_state(self, user_id: str) -> State:
+        return self.conn.hget(user_id, 'state')
     
-    def get_data(self, user_id) -> dict:
-        pass
+    def set_data(self, user_id: str, data: str):
+        self.conn.hset(f'{user_id}', 'data', data)
+        ttl = timedelta(minutes=10)
+        self.conn.expire(name=f'{user_id}', time=ttl)
+    
+    def get_data(self, user_id: str) -> dict:
+        return self.conn.hgetall(user_id)
--- bot/src/db/redis.py
+++ bot/src/db/redis.py
@@ -3,17 +3,19 @@ from redis.exceptions import ConnectionError
 from loguru import logger
 from src.components.config import RedisDB
 
-def create_connectrion(config: RedisDB, decode_responses=True):
-    connection = redis.Redis(
+def create_connectrion(config: RedisDB, decode_responses=True, charset="utf-8"):
+    connection = redis.StrictRedis(
         host = config.host,
         port = config.port,
-        decode_responses = decode_responses
+        decode_responses = decode_responses,
+        charset = charset
     )
     try:
         connection.ping()
         logger.info(f'{config.host}:{config.port} - Connection to Redis DB successful')
-        return connection
     except ConnectionError as e:
         logger.error(f'{config.host}:{config.port} - {e}')
         logger.error('Application was not started')
         raise ConnectionError
+    else: 
+        return connection
