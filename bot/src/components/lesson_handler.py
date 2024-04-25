44e2934a1c5c1a97d694bbfd69f44272786b3344
k1cker666 <pomoymu@gmail.com>
2024-04-25 22:20:36 +0300

    refactor tgbot: remove second parse data in check_answer

--- bot/src/components/lesson_handler.py
+++ bot/src/components/lesson_handler.py
@@ -37,21 +37,19 @@ class LessonHandler:
         context: ContextTypes.DEFAULT_TYPE,
         callback_data: CallbackData
         ):
-        query = update.callback_query
         if callback_data.cb_type == self.CallBackType.init_lesson.name:
-            await self.__start_lesson(update, context, query)
+            await self.__start_lesson(update, context)
         if callback_data.cb_type == self.CallBackType.check_answer.name:
-            await self.__check_answer(query)
+            await self.__check_answer(update, callback_data)
                 
     def create_answer_button(self, word: str) -> InlineKeyboardButton:
         return InlineKeyboardButton(
             text=word,
             callback_data=CallbackData(
-                cb_processor=f'{self.name}',
-                cb_type=f'{self.CallBackType.check_answer.name}',
-                word=f'{word}'
-            ).to_string()
-            )
+                cb_processor=self.name,
+                cb_type=self.CallBackType.check_answer.name,
+                word=word
+            ).to_string())
         
     def create_answers_menu(self, question: Question) -> InlineKeyboardMarkup:
         buttons = []
@@ -63,9 +61,9 @@ class LessonHandler:
     async def __start_lesson(
         self,
         update: Update,
-        context: ContextTypes.DEFAULT_TYPE,
-        query: CallbackQuery
+        context: ContextTypes.DEFAULT_TYPE
         ):
+        query = update.callback_query
         await query.delete_message()
         data = self.lesson_init_processor.init()
         self.user_state_processor.set_data(user_id='kicker', data=data.model_dump_json())
@@ -76,17 +74,16 @@ class LessonHandler:
             text=f"Переведи слово {data.questions[data.active_question]['word_to_translate']}",
             reply_markup=reply_markup)
     
-    async def __check_answer(self, query: CallbackQuery):
+    async def __check_answer(self, update: Update, callback_data: CallbackData):
         data = LessonDTO.model_validate_json(self.user_state_processor.get_data(user_id='kicker'))
-        callback: CallbackData = CallbackData.from_string(query.data)
-        if callback.word == data.questions[data.active_question]['correct_answer']:
+        if callback_data.word == data.questions[data.active_question]['correct_answer']:
             if self.__have_next_question(data.active_question, data.questions):
                 data = self.__update_active_question(user_id='kicker', data=data)
-                await self.__send_next_question(query=query, data=data)
+                await self.__send_next_question(update=update, data=data)
             else:
-                await self.__end_lesson(query=query, user_id='kicker')
+                await self.__end_lesson(update=update, user_id='kicker')
         else:
-            await self.__send_same_question(query=query, data=data)
+            await self.__send_same_question(update=update, data=data)
             
     def __have_next_question(
         self,
@@ -100,16 +97,19 @@ class LessonHandler:
         self.user_state_processor.set_data(user_id=user_id, data=data.model_dump_json())
         return data
     
-    async def __send_next_question(self, query: CallbackQuery, data: LessonDTO):
+    async def __send_next_question(self, update: Update, data: LessonDTO):
+        query = update.callback_query
         reply_markup = self.create_answers_menu(question=data.questions[data.active_question])
         await query.edit_message_text(f"Перевод верный\nСледующее слово {data.questions[data.active_question]['word_to_translate']}")
         await query.edit_message_reply_markup(reply_markup)
         
-    async def __end_lesson(self, query: CallbackQuery, user_id:str):
+    async def __end_lesson(self, update: Update, user_id:str):
+        query = update.callback_query
         await query.edit_message_text('Все слова урока переведены')
         self.user_state_processor.set_state(user_id=user_id, state=State.lesson_inactive)
         
-    async def __send_same_question(self, query: CallbackQuery, data: LessonDTO):
+    async def __send_same_question(self, update: Update, data: LessonDTO):
+        query = update.callback_query
         reply_markup = self.create_answers_menu(question=data.questions[data.active_question])
         await query.edit_message_text(f"Перевод неверный.\nПопробуй еще раз перевести слово {data.questions[data.active_question]['word_to_translate']}")
         await query.edit_message_reply_markup(reply_markup)
