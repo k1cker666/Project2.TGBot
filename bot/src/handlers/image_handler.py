9fd17b4b8f59ebe9d4d02434f94f34a45ffca880
k1cker666 <pomoymu@gmail.com>
2024-05-03 16:28:00 +0300

    feat tgbot: add get_image methods in image_handler

--- bot/src/handlers/image_handler.py
+++ bot/src/handlers/image_handler.py
@@ -19,3 +19,38 @@ class ImageHandler:
         buffer.seek(0)
         return buffer
         
+    def get_start_image(self, word: str) -> BytesIO:
+        start_image = self.start_image.copy()
+        draw = ImageDraw.Draw(start_image)
+        draw.text(
+            xy=(425, 345),
+            text=word,
+            fill=(0,0,0),
+            font=self.font,
+            anchor='mm'
+        )
+        return self.write_in_buffer(start_image)
+    
+    def get_right_answer_image(self, word: str) -> BytesIO:
+        right_answer = self.right_answer.copy()
+        draw = ImageDraw.Draw(right_answer)
+        draw.text(
+            xy=(425, 345),
+            text=word,
+            fill=(0,0,0),
+            font=self.font,
+            anchor='mm'
+        )
+        return self.write_in_buffer(right_answer)
+    
+    def get_wrong_answer_image(self, word: str) -> BytesIO:
+        wrong_answer = self.wrong_answer.copy()
+        draw = ImageDraw.Draw(wrong_answer)
+        draw.text(
+            xy=(425, 345),
+            text=word,
+            fill=(0,0,0),
+            font=self.font,
+            anchor='mm'
+        )
+        return self.write_in_buffer(wrong_answer)
