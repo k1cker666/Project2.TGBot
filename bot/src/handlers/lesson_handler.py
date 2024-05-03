19c186aa6c0c0e9bb24b00ff3fc8423d7f16493f
k1cker666 <pomoymu@gmail.com>
2024-05-03 17:36:56 +0300

    feat tgbot: images in lesson

--- bot/src/handlers/lesson_handler.py
+++ bot/src/handlers/lesson_handler.py
@@ -2,12 +2,13 @@ from enum import Enum, auto
 from telegram import (
     Update,
     InlineKeyboardButton,
-    InlineKeyboardMarkup
+    InlineKeyboardMarkup,
+    InputMedia
 )
 from telegram.ext import ContextTypes
 from src.components.lesson_init_processor import LessonInitProcessor
 from src.components.user_state_processor import UserStateProcessor, State
-from bot.src.handlers.image_handler import ImageHandler
+from src.handlers.image_handler import ImageHandler
 from src.models.callback import CallbackData
 from src.models.lesson_dto import LessonDTO, Question
 from src.helpfuncs.menu import build_menu
@@ -42,7 +43,7 @@ class LessonHandler:
         if callback_data.cb_type == self.CallBackType.init_lesson.name:
             await self.__start_lesson(update, context)
         if callback_data.cb_type == self.CallBackType.check_answer.name:
-            await self.__check_answer(update, callback_data)
+            await self.__check_answer(update, context, callback_data)
                 
     def create_answer_button(self, word: str) -> InlineKeyboardButton:
         return InlineKeyboardButton(
@@ -66,10 +67,13 @@ class LessonHandler:
         self.user_state_processor.set_data(user_id=update.effective_user.username, data=data.model_dump_json())
         self.user_state_processor.set_state(user_id=update.effective_user.username, state=State.lesson_active)
         reply_markup = self.create_answers_menu(question=data.questions[data.active_question])
-        await context.bot.send_message(
+        photo_buffer = self.image_handler.get_start_image(data.questions[data.active_question]['word_to_translate'].capitalize())
+        await context.bot.send_photo(
             chat_id=update.effective_chat.id,
-            text=f"Переведи слово {data.questions[data.active_question]['word_to_translate'].capitalize()}",
-            reply_markup=reply_markup)
+            photo=photo_buffer,
+            reply_markup=reply_markup
+        )
+        photo_buffer.close()
     
     def __have_next_question(self, active_question: int, questions_pool: int) -> bool:
         return active_question+1 != len(questions_pool)
@@ -79,30 +83,45 @@ class LessonHandler:
         self.user_state_processor.set_data(user_id=user_id, data=data.model_dump_json())
         return data
     
-    async def __end_lesson(self, update: Update):
+    async def __end_lesson(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         query = update.callback_query
-        await query.edit_message_text('Все слова урока переведены')
+        await query.delete_message()
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Все слова урока переведены'
+        )
         self.user_state_processor.set_state(user_id=update.effective_user.username, state=State.lesson_inactive)
     
-    async def __send_next_question(self, update: Update, data: LessonDTO):
+    async def __send_next_question(self, update: Update, context: ContextTypes.DEFAULT_TYPE, data: LessonDTO):
         query = update.callback_query
+        await query.delete_message()
         reply_markup = self.create_answers_menu(question=data.questions[data.active_question])
-        await query.edit_message_text(f"Перевод верный\nСледующее слово {data.questions[data.active_question]['word_to_translate'].capitalize()}")
-        await query.edit_message_reply_markup(reply_markup)
-    
-    async def __send_same_question(self, update: Update, data: LessonDTO):
+        photo_buffer = self.image_handler.get_right_answer_image(data.questions[data.active_question]['word_to_translate'].capitalize())
+        await context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=photo_buffer,
+            reply_markup=reply_markup
+        )
+        photo_buffer.close()
+        
+    async def __send_same_question(self, update: Update, context: ContextTypes.DEFAULT_TYPE, data: LessonDTO):
         query = update.callback_query
+        await query.delete_message()
         reply_markup = self.create_answers_menu(question=data.questions[data.active_question])
-        await query.edit_message_text(f"Перевод неверный.\nПопробуй еще раз перевести слово {data.questions[data.active_question]['word_to_translate'].capitalize()}")
-        await query.edit_message_reply_markup(reply_markup)
+        photo_buffer = self.image_handler.get_wrong_answer_image(data.questions[data.active_question]['word_to_translate'].capitalize())
+        await context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=photo_buffer,
+            reply_markup=reply_markup
+        )
     
-    async def __check_answer(self, update: Update, callback_data: CallbackData):
+    async def __check_answer(self, update: Update, context: ContextTypes.DEFAULT_TYPE, callback_data: CallbackData):
         data = LessonDTO.model_validate_json(self.user_state_processor.get_data(user_id=update.effective_user.username))
         if callback_data.word.lower() == data.questions[data.active_question]['correct_answer']:
             if self.__have_next_question(data.active_question, data.questions):
                 data = self.__update_active_question(user_id=update.effective_user.username, data=data)
-                await self.__send_next_question(update=update, data=data)
+                await self.__send_next_question(update=update, context=context, data=data)
             else:
-                await self.__end_lesson(update=update)
+                await self.__end_lesson(update=update, context=context)
         else:
-            await self.__send_same_question(update=update, data=data)
+            await self.__send_same_question(update=update, context=context, data=data)
