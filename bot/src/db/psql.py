4807a893b2bfaa9a4f815c40db69a357dcae3205
k1cker666 <pomoymu@gmail.com>
2024-05-04 11:33:49 +0300

    fix tgbot: fix for connection pool

--- bot/src/db/psql.py
+++ bot/src/db/psql.py
@@ -1,28 +1,22 @@
 import psycopg_pool
-import psycopg
 from psycopg import OperationalError
 from loguru import logger
 from src.components.config import PostgresDB
 
 def create_connection_pool(config: PostgresDB) -> psycopg_pool.ConnectionPool:
-    try:
-        connection = psycopg.connect(
-            dbname = config.dbname,
-            user = config.user,
-            password = config.password,
-            host = config.host,
-            port = config.port
+    pool = psycopg_pool.ConnectionPool(
+            conninfo = f'postgresql://{config.user}:{config.password}@{config.host}:{config.port}/{config.dbname}',
+            open=True,
+            max_size=10,
+            max_waiting=10
         )
+    try:
+        conn = pool.getconn()
     except OperationalError as e:
         logger.error(f'{config.host}:{config.port} - {e}')
         logger.info('Application was not started')
         raise OperationalError
     else:
-        connection.close()
-        pool = psycopg_pool.ConnectionPool(
-            conninfo = f'postgresql://{config.user}:{config.password}@{config.host}:{config.port}/{config.dbname}',
-            open=True,
-            max_size=10
-        )
+        pool.putconn(conn)
         logger.info(f'{config.host}:{config.port} - Connection to PostgreSQL as user {config.user} successful')
         return pool
