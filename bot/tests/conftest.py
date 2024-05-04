252a273f8174bad64b9364a518beb399bb538021
k1cker666 <pomoymu@gmail.com>
2024-05-04 15:47:03 +0300

    fix tgbot: fix for insserts in conftest

--- bot/tests/conftest.py
+++ bot/tests/conftest.py
@@ -30,68 +30,32 @@ def setup_users_table(psql_connect):
     with psql_connect.connection() as conn:
         with conn.cursor() as cur:
             cur.execute("""
-            do $$
-            begin
-                if not exists (
-                    select 1 from users
-                    where tg_login = '@k1cker666') then
-                    insert into users (tg_login, login, password, words_in_lesson, native_language, language_to_learn, word_level)
-                    VALUES ('@k1cker666', 'admin', 'admin', 15, 'ru', 'en', 'A1');
-                end if;
-            end
-            $$;""")
+            insert into users (tg_login, login, password, words_in_lesson, native_language, language_to_learn, word_level)
+            values ('@k1cker666', 'admin', 'admin', 4, 'ru', 'en', 'A1')
+            on conflict do nothing;
+            """)
         
 @pytest.fixture(scope="session")
 def setup_words_table(psql_connect):
     with psql_connect.connection() as conn:
         with conn.cursor() as cur:
             cur.execute("""
-            do $$
-            begin
-                if not exists (
-                    select 1 from words
-                    where word = 'привет') then
-                    insert into words (word_id, language, level, word) VALUES (1, 'ru', 'A1', 'привет');
-                end if;
-                if not exists (
-                    select 1 from words
-                    where word = 'hello') then
-                    insert into words (word_id, language, level, word) VALUES (1, 'en', 'A1', 'hello');
-                end if;
-                if not exists (
-                    select 1 from words
-                    where word = 'делать') then
-                    insert into words (word_id, language, level, word) values (2, 'ru', 'A1', 'делать');
-                end if;
-                if not exists (
-                    select 1 from words
-                    where word = 'do') then
-                    insert into words (word_id, language, level, word) values (2, 'en', 'A1', 'do');
-                end if;
-                if not exists (
-                    select 1 from words
-                    where word = 'думать') then
-                    insert into words (word_id, language, level, word) values (3, 'ru', 'A1', 'думать');
-                end if;
-                if not exists (
-                    select 1 from words
-                    where word = 'think') then
-                    insert into words (word_id, language, level, word) values (3, 'en', 'A1', 'think');
-                end if;
-                if not exists (
-                    select 1 from words
-                    where word = 'идти') then
-                    insert into words (word_id, language, level, word) values (4, 'ru', 'A1', 'идти');
-                end if;
-                if not exists (
-                    select 1 from words
-                    where word = 'go') then
-                    insert into words (word_id, language, level, word) values (4, 'en', 'A1', 'go');
-                end if;
-                if not exists (
-                    select 1 from words_in_progress
-                    where word_id = 1 and user_id = 1 and language = 'en') then
-                    insert into words_in_progress (user_id, word_id, language, number_of_repetitions) values (1, 1, 'en', 3);
-                end if;
-            end
-            $$;""")
+            insert into words (word_id, language, level, word) VALUES (1, 'ru', 'A1', 'привет')
+            on conflict do nothing;
+            insert into words (word_id, language, level, word) VALUES (1, 'en', 'A1', 'hello')
+            on conflict do nothing;
+            insert into words (word_id, language, level, word) values (2, 'ru', 'A1', 'делать')
+            on conflict do nothing;
+            insert into words (word_id, language, level, word) values (2, 'en', 'A1', 'do')
+            on conflict do nothing;
+            insert into words (word_id, language, level, word) values (3, 'ru', 'A1', 'думать')
+            on conflict do nothing;
+            insert into words (word_id, language, level, word) values (3, 'en', 'A1', 'think')
+            on conflict do nothing;
+            insert into words (word_id, language, level, word) values (4, 'ru', 'A1', 'идти')
+            on conflict do nothing;
+            insert into words (word_id, language, level, word) values (4, 'en', 'A1', 'go')
+            on conflict do nothing;
+            insert into words_in_progress (user_id, word_id, language, number_of_repetitions) values (1, 1, 'en', 3)
+            on conflict do nothing;
+            """)
