36f4cbbb485af9565e58d65cb5ec93abad6d10e2
k1cker666 <pomoymu@gmail.com>
2024-05-07 22:05:51 +0300

    fix tgbot: new logger

--- bot/main.py
+++ bot/main.py
@@ -2,19 +2,17 @@ from src import bot
 from src.dependencies import DependenciesBuilder
 from psycopg import OperationalError
 from redis.exceptions import ConnectionError
-from loguru import logger
+from src.log import *
 
 def main():
     try:
         deps = DependenciesBuilder.build()
         bot.start_bot(deps)
+        deps.close()
     except OperationalError:
         return
     except ConnectionError:
         return
-    else:
-        deps.close()
-        logger.info("Application was stopped")
     
 if __name__ == "__main__":
     main()
--- bot/src/log.py
+++ bot/src/log.py
@@ -1,3 +1,23 @@
+import inspect
+import logging
 from loguru import logger
 
-logger.add(format="{time} {level} {message}", level="INFO")
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        
+        level: str | int
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+        
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO, force=True)
+logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger(__name__)
