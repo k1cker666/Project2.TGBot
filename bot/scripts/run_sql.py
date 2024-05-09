946987e857b9c468def45ee289d250492bf757e8
k1cker666 <pomoymu@gmail.com>
2024-05-09 09:19:53 +0300

    fix tgbot: fix for sql scripts

--- bot/scripts/run_sql.py
+++ bot/scripts/run_sql.py
@@ -1,46 +1,50 @@
 import json
 import os
-import sys
 import time
 
 from loguru import logger
 import psycopg
+import click
 
-file = sys.argv[1]
+@click.command()
+@click.option('--file', help='Name of file in folder /bot/scripts/sql/')
+def run_sql(file):
+    with open(f"{os.path.abspath(os.curdir)}/bot/scripts/sql/{file}") as sql_file:
+        text = sql_file.read()
+        commands = text.split('\n')
+        commands_count = len(commands)
 
-with open(f"{os.path.abspath(os.curdir)}/bot/scripts/sql/{file}") as sql_file:
-    text = sql_file.read()
-    commands = text.split('\n')
-    commands_count = len(commands)
-    
-json_dir = f"{os.path.abspath(os.curdir)}/bot/config/config.json"
-with open(json_dir, 'r') as file:
-    config = json.load(file)['psql']
+    json_dir = f"{os.path.abspath(os.curdir)}/bot/config/config.json"
+    with open(json_dir, 'r') as file:
+        config = json.load(file)['psql']
 
-try:
-    db_name = config['dbname']
-    db_user = config['user']
-    db_password = config['password']
-    db_host = config['host']
-    db_port =  config['port']
-    with psycopg.connect(
-        dbname = db_name,
-        user = db_user,
-        password = db_password,
-        host = db_host, 
-        port = db_port,
-        autocommit = True) as conn:
-        logger.info(f'{db_host}:{db_port} - Connection to {db_name} database as user {db_user} successful')
-        with conn.cursor() as cur:
-            for index, command in enumerate(commands):
-                cur.execute(command)
-                if index == 0:
-                    logger.info(f'{db_host}:{db_port} - Insertion started')
-                elif index%900 == 0:
-                    percent = index/commands_count*100
-                    logger.info(f'{db_host}:{db_port} - Insertion is {int(percent)}% complete')
-                time.sleep(0.5)
-        logger.info(f'{db_host}:{db_port} - Inserts {file} complete')
-            
-except psycopg.OperationalError as error:
-    logger.error(f"{db_host}:{db_port} - {error}")
+    try:
+        db_name = config['dbname']
+        db_user = config['user']
+        db_password = config['password']
+        db_host = config['host']
+        db_port =  config['port']
+        with psycopg.connect(
+            dbname = db_name,
+            user = db_user,
+            password = db_password,
+            host = db_host, 
+            port = db_port,
+            autocommit = True) as conn:
+            logger.info(f'{db_host}:{db_port} - Connection to {db_name} database as user {db_user} successful')
+            with conn.cursor() as cur:
+                for index, command in enumerate(commands):
+                    cur.execute(command)
+                    if index == 0:
+                        logger.info(f'{db_host}:{db_port} - Insertion started')
+                    elif index%900 == 0:
+                        percent = index/commands_count*100
+                        logger.info(f'{db_host}:{db_port} - Insertion is {int(percent)}% complete')
+                    time.sleep(0.5)
+            logger.info(f'{db_host}:{db_port} - Inserts {file} complete')
+
+    except psycopg.OperationalError as error:
+        logger.error(f"{db_host}:{db_port} - {error}")
+        
+if __name__ == "__main__":
+    run_sql()
