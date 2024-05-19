c3eafb7645afcd4208f8907c57c0d8f3528f41c7
k1cker666 <pomoymu@gmail.com>
2024-05-19 19:24:45 +0300

    feat backend: handle forms in html

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 import uuid
 from contextlib import asynccontextmanager
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, HTTPException
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from redis.exceptions import ConnectionError
 from src.dependencies import Dependencies, DependenciesBuilder
@@ -57,3 +59,38 @@ def auth(uuid_token: str):
     if deps.redis.is_valid_token(uuid_token=uuid_token):
         return FileResponse("backend/static/auth.html")
     return FileResponse("backend/static/access_denied.html")
+
+
+@app.post("/login/")
+async def login(
+    username: Annotated[str, Form()], password: Annotated[str, Form()]
+):
+    if username != "1" or password != "1":
+        raise HTTPException(
+            status_code=401, detail="Неверное имя пользователя или пароль"
+        )
+    # TODO: Сделать проверку юзера
+    return {"message": "login succes"}
+
+
+@app.post("/register/")
+async def register(
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    confirm_password: Annotated[str, Form()],
+):
+    # TODO: Сделать проверку зарегестрирован ли уже логин
+    if password != confirm_password:
+        raise HTTPException(status_code=400, detail="Пароли не совпадают")
+    # TODO: Здесь нужно добавить регистрацию пользователя в базе данных
+    return {"message": "reg succces"}
+
+
+@app.get("/registration_complete/", response_class=HTMLResponse)
+async def registration_complete():
+    return FileResponse("backend/static/registration_complete.html")
+
+
+@app.get("/login_complete/", response_class=HTMLResponse)
+async def login_complete():
+    return FileResponse("backend/static/login_complete.html")
